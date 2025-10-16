90b7a7cd7f6472a99d3749bb8dd1eabb4adebe28
mhsabit11 <mhsabit101@gmail.com>
2025-10-16 13:59:06 +0600

    A properly masked animadet section is added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hero from "./components/Hero.jsx";
 import {Circ} from "gsap/gsap-core";
 import Cocktails from "./components/Cocktails.jsx";
 import About from "./components/About.jsx";
+import Art from "./components/Art.jsx";
  gsap.registerPlugin(ScrollTrigger,SplitText);
 
 
@@ -16,6 +17,7 @@ const App = () => {
             <Hero />
             <Cocktails />
             <About />
+            <Art />
 
         </main>
     )
